b86f2a74d103a0dae2c9f265858522bd01489993
ChanX21 <47290661+ChanX21@users.noreply.github.com>
2020-07-11 15:42:54 +0530

    Update plot3.r

--- plot3.r
+++ plot3.r
@@ -24,4 +24,4 @@ plot(Subdata$datetime,Subdata$Sub_metering_1,type = 'l',ylab = "Energy Sub Meter
 lines(Subdata$datetime,Subdata$Sub_metering_2,type = 'l',col = 'red')
 lines(Subdata$datetime,Subdata$Sub_metering_3,type = 'l',col = 'blue')
 
-legend('topright', c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2, col=c("black", "red", "blue"),cex = 0.45)
+legend('topright', c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2, col=c("black", "red", "blue"),cex = 0.3)
